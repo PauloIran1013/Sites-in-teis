99b897c3c049291b076d5955d75e5cbf49f90e4e
PauloIran1013 <julioirancoelho@gmail.com>
2024-10-16 13:46:18 -0300

    asaasa

--- anel_do_eldinho/app.js
+++ anel_do_eldinho/app.js
@@ -38,7 +38,7 @@ app.delete('/jogadores', async(req,res)=>{
 })
 
 app.get('/personagem', async(req,res)=>{
-    const personagem = await knexdb ('personagem').select('*')
+    const personagem = await knexdb ('personagem').select('*') 
     res.status(200).json({personagem})
 })
 
@@ -57,11 +57,11 @@ app.get('/personagem', async(req,res)=>{
     res.status(200).json({personagem})
  })
 
- app.delete('/personagem', async(req,res)=>{
-    const {id_personagem} = req.body
-    const personagem = await knexdb ('personagem').select('*').del()
-    res.send('Nunca é um adeus.')
- })
+ app.delete('/personagem', async (req, res) => {
+    const { id_personagem } = req.body;
+    const personagem = await knexdb('personagem').where({ id_personagem }).del();
+    res.send('Nunca é um adeus.');
+});
 
  app.get('/missoes', async(req,res)=>{
     const missoes = await knexdb ('missoes').select('*')
@@ -74,11 +74,11 @@ app.get('/personagem', async(req,res)=>{
     res.status(200).json({missoes})
 })
 
-app.delete('/missoes', async(req,res)=>{
-    const {id_missao} = req.body
-    const missoes = await knexdb ('missoes').select('*').del()
-    res.send("Ja era")
-})
+app.delete('/personagem', async (req, res) => {
+    const { id_personagem } = req.body;
+    const personagem = await knexdb('personagem').where({ id_personagem }).del();
+    res.send('Nunca é um adeus.');
+});
 
 app.get('/missoes/:id_missao', async(req,res)=>{
     const {id_missao} = req.params
@@ -87,6 +87,57 @@ app.get('/missoes/:id_missao', async(req,res)=>{
       res.send('Missão não encontrada')
     }
     res.status(200).json({missoes})
+})
+
+app.get('/itens',async (req,res)=>{
+    const itens = await knexdb ('itens').select('*')
+    res.status(200).json({itens})
+})
+
+app.post('/itens',async(req,res)=>{
+    const {nome,descricao,tipo,efeito} = req.body
+    const itens = await knexdb ('itens').select('*').insert({nome,descricao,tipo,efeito})
+    res.status(200).json({itens})
+})
+
+app.delete('/itens', async (req, res) => {
+    const { id_item } = req.body;
+    const itens = await knexdb('itens').where({ id_item }).del();
+    res.send('Item deletado');
+});
+ app.get('/itens/:id_item', async(req,res)=>{
+    const {id_item} = req.params
+    const itens = await knexdb ('itens').select('*').where({id_item})
+    if (itens == '') {
+        res.send('Item não encontrada')
+    }
+    res.status(200).json({itens})
+ })
+
+app.get('/inventarios',async(req,res)=>{
+    const inventario = await knexdb('inventarios').select('*')
+    res.status(200).json({inventario})
+})
+
+app.post('/inventarios', async(req,res)=>{
+    const {id_personagem,id_item,quantidade} = req.body
+    const inventario = await knexdb ('inventarios').select('*').insert({id_personagem,id_item,quantidade})
+    res.status(200).json({inventario})
+})
+
+app.delete('/inventarios',async(req,res)=>{
+    const {id_inventario} = req.body
+    const inventario = await knexdb ('inventarios').where().del()
+    res.send('Deletado')
+})
+
+app.get('/inventarios/:id_inventario',async(req,res)=>{
+    const {id_inventario} = req.params
+    const inventario = await knexdb('inventarios').select('*').where({id_inventario})
+    if (inventario == '') {
+        res.send("Não encontrado")
+    }
+    res.status(200).json({inventario})
 })
 
 
